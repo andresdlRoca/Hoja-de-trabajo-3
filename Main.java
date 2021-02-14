271a89b78878494bdf11fc116c95960d6e7b5e2a
Andrés de la Roca <andresdelaroca11@gmail.com>
2021-02-14 21:34:43 +0000

    Added Sorting.test, Modified Main.java, NumGeneral.java, Numeros.java and Sorting.java

--- Main.java
+++ Main.java
@@ -1,123 +1,118 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
 *@author Pedro Arriola 20188
 *@author Esteban Aldana 20591
 *@author Andrés de la Roca 20332
 */
-import java.util.Random;
-import java.io.FileWriter;
-import java.io.IOExeption;
-import java.util.Scanner;
 
 public class Main {
-  
-  /**
-  *
-  *@param String[] args
-  */
-  public static void main(String[] args) {
 
-    Sorting ob = new Sorting();
+    /**
+     *Metodo main del programa
+     *@param String[] args
+     */
+    public static void main(String[] args) {
 
-    try {
-      
-    Random rand = new Random();
-    FileWriter writer = new FileWriter("data.txt");
-    int[] array = new int[10];
+        Sorting ob = new Sorting();
 
-    for(int i = 0; i < array.length; i++) {
-      array[i] = rand.nextInt();
-      System.out.println(array[i]);
-      writer.write(array[i] + " ");
-    }
-    writer.close();
+        ArrayList<Integer> numList = new ArrayList<Integer>();
 
-    } catch (IOException e) {
-      System.err.println("El archivo no se ha encontrado")
-    }
-    int op = 0;
-    boolean salir = false;
+        NumGenerator.generate(numList);
 
-    Scanner scan = new Scanner(System.in);
+        int op = 0;
+        boolean salir = false;
 
-    while (!salir){
-      
-      //Menu para escoger el sort a trabajar
-      System.out.println("\nBienvenido Seleccione el sorting que desea realizar ");
-              System.out.println("1. Gnome sort");
-              System.out.println("2. Merge sort");
-              System.out.println("3. Quick sort");
-              System.out.println("4. Radix Sort");
-              System.out.println("5. Bubble sort");
-              System.out.println("6. Salir\n");
-              
-              op = scan.nextInt();
+        Scanner scan = new Scanner(System.in);
 
-              if(op == 1){ //Gnome Sort
-                int arr[] = { 34, 2, 10, -9 }; 
-  
-                gnomeSort(arr, arr.length); 
-  
-                System.out.print("Secuencia despues de aplicar Gnome sort "); 
-                 System.out.println(Arrays.toString(arr)); 
-                 
-              } else if (op == 2) { //Merge Sort
+        int cont =0;
 
-                 int arr[] = { 12, 11, 13, 5, 6, 7 };
-        
-                System.out.println("Array ");
-                printArray(arr);
-        
-                //Sorting ob = new Sorting();
-                ob.sort(arr, 0, arr.length - 1);
-        
-                System.out.println("\nArray ordenado ");
-                printArray(arr);
+        Numeros[] data = new Numeros[10];
+        for(int i=0;i<10;i++){
+            //String numCadena = String.valueOf(numeros.get(i));
+            data[i] = new Numeros (numList.get(i));
+        }
 
-              } else if (op == 3) { //Quick Sort
-                int arr[] = {10, 7, 8, 9, 1, 5}; 
-                int n = arr.length; 
-          
-                Sorting ob = new Sorting(); 
-                ob.sort(arr, 0, n-1); 
-          
-                System.out.println("Array Ordenado"); 
-                printArray(arr); 
-                
+        while (!salir){
 
-              } else if (op == 4) { //Radix Sort
+            //Menu para escoger el sort a trabajar
+            System.out.println("\nBienvenido, Seleccione el sorting que desea realizar ");
+            System.out.println("1. Gnome sort");
+            System.out.println("2. Merge sort");
+            System.out.println("3. Quick sort");
+            System.out.println("4. Radix Sort");
+            System.out.println("5. Bubble sort");
+            System.out.println("6. Salir\n");
 
-                int arr[] = {14, 8, 9, 20, 99, 50, 6, 2, 1}
+            op = scan.nextInt();
 
-                System.out.println("Array (Radix)");
-                printArray(arr);
+            if(op == 1){ //Gnome Sort
+                String res = Sorting.gnomeSort(data);
+                System.out.println(res);
+                for(Numeros ordenado : data){
+                    System.out.println(ordenado);
+                    cont++;
+                }
+                System.out.println("Gnome sort");
+                System.out.println("Cantidad de datos: " + cont);
+                cont = 0;
 
-                int length = arr.length;
-                ob.radixSort(arr, length);
+            } else if (op == 2) { //Merge Sort
+                Comparable[] ordenado = Sorting.mergeSort(data);
+                for(Comparable orden : ordenado){
+                    System.out.println(orden);
+                    cont++;
+                }
+                System.out.println("Merge Sort");
+                System.out.println("Cantidad de datos: " + cont);
+                cont = 0;
 
-                System.out.println("\nArray Ordenado (Radix)");
-                printArray(arr);
+            } else if (op == 3) { //Quick Sort
+                String res = Sorting.quickSort(data,0, data.length-1);
+                System.out.println(res);
+                for(Numeros ordenado : data){
+                    System.out.println(ordenado);
+                    cont++;
+                }
+                System.out.println("Quick Sort");
+                System.out.println("Cantidad de datos: " + cont);
+                cont = 0;
 
-              } else if (op == 5) { //Bubble Sort
+            } else if (op == 4) { //Radix Sort
+                String res = Sorting.radixSort(data, data.length);
+                System.out.println(res);
+                for(Numeros ordenado : data){
+                    System.out.println(ordenado);
+                    cont++;
+                }
+                System.out.println("Radix sort");
+                System.out.println("Cantidad de datos: " + cont);
+                cont = 0;
 
-                int arr[] = {9, 5, 18, 22, 30, 1, 12, 4}
 
-                System.out.println("Array (Bubble)");
-                printArray(arr);
+            } else if (op == 5) { //Bubble Sort
+                String res = Sorting.bubbleSort(data);
+                System.out.println(res);
+                for(Numeros ordenado : data){
+                    System.out.println(ordenado);
+                    cont++;
+                }
+                System.out.println("Bubble Sort");
+                System.out.println("Cantidad de datos: " + cont);
+                cont = 0;
 
-                ob.bubbleSort(arr);
-
-                System.out.println("\nArray ordenado (Bubble)");
-                printArray(arr);
-
-              } else if (op == 6) { //Salida
+            } else if (op == 6) { //Salida
+                System.out.println("Gracias por usar nuestro Programa");
                 salir = true;
-              } else { //Opcion invalida
+
+            } else { //Opcion invalida
                 System.out.println("Ingrese una opcion valida");
-              }
+            }
+        }
+
+
     }
-    
-    
-  }
 
 }
--- Numeros.java
+++ Numeros.java
@@ -1,3 +1,30 @@
-public class Numero implements Comparable<Numero>{
-  private 
+/**
+*@author Pedro Arriola 20188
+*@author Esteban Aldana 20591
+*@author Andrés de la Roca 20332
+*/
+
+public class Numeros implements Comparable<Numeros>{
+    private Integer finalNum;
+
+    public Numeros(Integer value){
+        finalNum = value;
+    }
+
+    public String toString(){
+
+        return String.valueOf(finalNum);
+    }
+
+    public int compareTo(Numeros num){
+        Integer result;
+        Integer otroDato = (num).getlastNum();
+        result=otroDato.compareTo(finalNum);
+
+        return result;
+    }
+
+    public Integer getlastNum(){
+        return finalNum;
+    }
 }
